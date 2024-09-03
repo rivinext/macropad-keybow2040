7d008e41218da3fe14a4a1b088b83de0bcdb5601
rivi <50409554+rivinext@users.noreply.github.com>
2024-09-03 19:10:37 +0900

    Add files via upload

--- layers.py
+++ layers.py
@@ -85,8 +85,8 @@ LAYER_4 = {
 }
 
 # None
-LAYER_5_COLOR_1 = (120, 155, 0)
-LAYER_5_COLOR_2 = (5, 255, 220)
+LAYER_5_COLOR_1 = (240,230,140)
+LAYER_5_COLOR_2 = (138,43,226)
 
 LAYER_5 = {
     'name': 'LAYER_5',
@@ -105,8 +105,8 @@ LAYER_5 = {
 }
 
 # None
-LAYER_6_COLOR_1 = (120, 155, 0)
-LAYER_6_COLOR_2 = (5, 255, 220)
+LAYER_6_COLOR_1 = (120, 215, 10)
+LAYER_6_COLOR_2 = (115, 65, 40)
 
 LAYER_6 = {
     'name': 'LAYER_6',
@@ -125,8 +125,8 @@ LAYER_6 = {
 }
 
 # None
-LAYER_7_COLOR_1 = (120, 155, 0)
-LAYER_7_COLOR_2 = (5, 255, 220)
+LAYER_7_COLOR_1 = (60, 155, 90)
+LAYER_7_COLOR_2 = (225, 55, 20)
 
 LAYER_7 = {
     'name': 'LAYER_7',
@@ -145,8 +145,8 @@ LAYER_7 = {
 }
 
 # None
-LAYER_8_COLOR_1 = (120, 155, 0)
-LAYER_8_COLOR_2 = (5, 255, 220)
+LAYER_8_COLOR_1 = (20, 155, 110)
+LAYER_8_COLOR_2 = (25, 55, 20)
 
 LAYER_8 = {
     'name': 'LAYER_8',
@@ -155,6 +155,146 @@ LAYER_8 = {
         4: LAYER_8_COLOR_2, 5: LAYER_8_COLOR_2, 6: LAYER_8_COLOR_2, 7: LAYER_8_COLOR_2,
         8: LAYER_8_COLOR_1, 9: LAYER_8_COLOR_1, 10: LAYER_8_COLOR_1, 11: LAYER_8_COLOR_1,
         12: LAYER_8_COLOR_2, 13: LAYER_8_COLOR_2, 14: LAYER_8_COLOR_2, 15: LAYER_8_COLOR_2
+    },
+    'keycodes': [
+        None, Keycode.A, Keycode.Q, Keycode.B,
+        Keycode.W, Keycode.C, Keycode.E, Keycode.D,
+        Keycode.R, Keycode.E, Keycode.T, Keycode.F,
+        Keycode.Y, Keycode.G, Keycode.U, Keycode.H
+    ]
+}
+
+# None
+LAYER_9_COLOR_1 = (255,99,71)
+LAYER_9_COLOR_2 = (30,144,255)
+
+LAYER_9 = {
+    'name': 'LAYER_9',
+    'color_map': {
+        0: LAYER_9_COLOR_1, 1: LAYER_9_COLOR_1, 2: LAYER_9_COLOR_1, 3: LAYER_9_COLOR_1,
+        4: LAYER_9_COLOR_2, 5: LAYER_9_COLOR_2, 6: LAYER_9_COLOR_2, 7: LAYER_9_COLOR_2,
+        8: LAYER_9_COLOR_1, 9: LAYER_9_COLOR_1, 10: LAYER_9_COLOR_1, 11: LAYER_9_COLOR_1,
+        12: LAYER_9_COLOR_2, 13: LAYER_9_COLOR_2, 14: LAYER_9_COLOR_2, 15: LAYER_9_COLOR_2
+    },
+    'keycodes': [
+        None, Keycode.A, Keycode.Q, Keycode.B,
+        Keycode.W, Keycode.C, Keycode.E, Keycode.D,
+        Keycode.R, Keycode.E, Keycode.T, Keycode.F,
+        Keycode.Y, Keycode.G, Keycode.U, Keycode.H
+    ]
+}
+
+# None
+LAYER_10_COLOR_1 = (218,112,214)
+LAYER_10_COLOR_2 = (50,205,50)
+
+LAYER_10 = {
+    'name': 'LAYER_10',
+    'color_map': {
+        0: LAYER_10_COLOR_1, 1: LAYER_10_COLOR_1, 2: LAYER_10_COLOR_1, 3: LAYER_10_COLOR_1,
+        4: LAYER_10_COLOR_2, 5: LAYER_10_COLOR_2, 6: LAYER_10_COLOR_2, 7: LAYER_10_COLOR_2,
+        8: LAYER_10_COLOR_1, 9: LAYER_10_COLOR_1, 10: LAYER_10_COLOR_1, 11: LAYER_10_COLOR_1,
+        12: LAYER_10_COLOR_2, 13: LAYER_10_COLOR_2, 14: LAYER_10_COLOR_2, 15: LAYER_10_COLOR_2
+    },
+    'keycodes': [
+        None, Keycode.A, Keycode.Q, Keycode.B,
+        Keycode.W, Keycode.C, Keycode.E, Keycode.D,
+        Keycode.R, Keycode.E, Keycode.T, Keycode.F,
+        Keycode.Y, Keycode.G, Keycode.U, Keycode.H
+    ]
+}
+
+# None
+LAYER_11_COLOR_1 = (255,105,180)
+LAYER_11_COLOR_2 = (75,0,130)
+
+LAYER_11 = {
+    'name': 'LAYER_11',
+    'color_map': {
+        0: LAYER_11_COLOR_1, 1: LAYER_11_COLOR_1, 2: LAYER_11_COLOR_1, 3: LAYER_11_COLOR_1,
+        4: LAYER_11_COLOR_2, 5: LAYER_11_COLOR_2, 6: LAYER_11_COLOR_2, 7: LAYER_11_COLOR_2,
+        8: LAYER_11_COLOR_1, 9: LAYER_11_COLOR_1, 10: LAYER_11_COLOR_1, 11: LAYER_11_COLOR_1,
+        12: LAYER_11_COLOR_2, 13: LAYER_11_COLOR_2, 14: LAYER_11_COLOR_2, 15: LAYER_11_COLOR_2
+    },
+    'keycodes': [
+        None, Keycode.A, Keycode.Q, Keycode.B,
+        Keycode.W, Keycode.C, Keycode.E, Keycode.D,
+        Keycode.R, Keycode.E, Keycode.T, Keycode.F,
+        Keycode.Y, Keycode.G, Keycode.U, Keycode.H
+    ]
+}
+
+# None
+LAYER_12_COLOR_1 = (120, 55, 225)
+LAYER_12_COLOR_2 = (45, 125, 40)
+
+LAYER_12 = {
+    'name': 'LAYER_12',
+    'color_map': {
+        0: LAYER_12_COLOR_1, 1: LAYER_12_COLOR_1, 2: LAYER_12_COLOR_1, 3: LAYER_12_COLOR_1,
+        4: LAYER_12_COLOR_2, 5: LAYER_12_COLOR_2, 6: LAYER_12_COLOR_2, 7: LAYER_12_COLOR_2,
+        8: LAYER_12_COLOR_1, 9: LAYER_12_COLOR_1, 10: LAYER_12_COLOR_1, 11: LAYER_12_COLOR_1,
+        12: LAYER_12_COLOR_2, 13: LAYER_12_COLOR_2, 14: LAYER_12_COLOR_2, 15: LAYER_12_COLOR_2
+    },
+    'keycodes': [
+        None, Keycode.A, Keycode.Q, Keycode.B,
+        Keycode.W, Keycode.C, Keycode.E, Keycode.D,
+        Keycode.R, Keycode.E, Keycode.T, Keycode.F,
+        Keycode.Y, Keycode.G, Keycode.U, Keycode.H
+    ]
+}
+
+# None
+LAYER_13_COLOR_1 = (70,130,180)
+LAYER_13_COLOR_2 = (255,228,225)
+
+LAYER_13 = {
+    'name': 'LAYER_13',
+    'color_map': {
+        0: LAYER_13_COLOR_1, 1: LAYER_13_COLOR_1, 2: LAYER_13_COLOR_1, 3: LAYER_13_COLOR_1,
+        4: LAYER_13_COLOR_2, 5: LAYER_13_COLOR_2, 6: LAYER_13_COLOR_2, 7: LAYER_13_COLOR_2,
+        8: LAYER_13_COLOR_1, 9: LAYER_13_COLOR_1, 10: LAYER_13_COLOR_1, 11: LAYER_13_COLOR_1,
+        12: LAYER_13_COLOR_2, 13: LAYER_13_COLOR_2, 14: LAYER_13_COLOR_2, 15: LAYER_13_COLOR_2
+    },
+    'keycodes': [
+        None, Keycode.A, Keycode.Q, Keycode.B,
+        Keycode.W, Keycode.C, Keycode.E, Keycode.D,
+        Keycode.R, Keycode.E, Keycode.T, Keycode.F,
+        Keycode.Y, Keycode.G, Keycode.U, Keycode.H
+    ]
+}
+
+# None
+LAYER_14_COLOR_1 = (20, 225, 170)
+LAYER_14_COLOR_2 = (225, 145, 0)
+
+LAYER_14 = {
+    'name': 'LAYER_14',
+    'color_map': {
+        0: LAYER_14_COLOR_1, 1: LAYER_14_COLOR_1, 2: LAYER_14_COLOR_1, 3: LAYER_14_COLOR_1,
+        4: LAYER_14_COLOR_2, 5: LAYER_14_COLOR_2, 6: LAYER_14_COLOR_2, 7: LAYER_14_COLOR_2,
+        8: LAYER_14_COLOR_1, 9: LAYER_14_COLOR_1, 10: LAYER_14_COLOR_1, 11: LAYER_14_COLOR_1,
+        12: LAYER_14_COLOR_2, 13: LAYER_14_COLOR_2, 14: LAYER_14_COLOR_2, 15: LAYER_14_COLOR_2
+    },
+    'keycodes': [
+        None, Keycode.A, Keycode.Q, Keycode.B,
+        Keycode.W, Keycode.C, Keycode.E, Keycode.D,
+        Keycode.R, Keycode.E, Keycode.T, Keycode.F,
+        Keycode.Y, Keycode.G, Keycode.U, Keycode.H
+    ]
+}
+
+# None
+LAYER_15_COLOR_1 = (186,85,211)
+LAYER_15_COLOR_2 = (0,255,127)
+
+LAYER_15 = {
+    'name': 'LAYER_15',
+    'color_map': {
+        0: LAYER_15_COLOR_1, 1: LAYER_15_COLOR_1, 2: LAYER_15_COLOR_1, 3: LAYER_15_COLOR_1,
+        4: LAYER_15_COLOR_2, 5: LAYER_15_COLOR_2, 6: LAYER_15_COLOR_2, 7: LAYER_15_COLOR_2,
+        8: LAYER_15_COLOR_1, 9: LAYER_15_COLOR_1, 10: LAYER_15_COLOR_1, 11: LAYER_15_COLOR_1,
+        12: LAYER_15_COLOR_2, 13: LAYER_15_COLOR_2, 14: LAYER_15_COLOR_2, 15: LAYER_15_COLOR_2
     },
     'keycodes': [
         None, Keycode.A, Keycode.Q, Keycode.B,
@@ -174,7 +314,14 @@ LAYER_100 = {
         2: LAYER_5_COLOR_2,
         6: LAYER_6_COLOR_2,
         10: LAYER_7_COLOR_2,
-        14: LAYER_8_COLOR_2
+        14: LAYER_8_COLOR_2,
+        1: LAYER_9_COLOR_1,
+        5: LAYER_10_COLOR_1,
+        9: LAYER_11_COLOR_1,
+        13: LAYER_12_COLOR_1,
+        4: LAYER_13_COLOR_1,
+        8: LAYER_14_COLOR_1,
+        12: LAYER_15_COLOR_1
     },
     'keycodes': None
 }
